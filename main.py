6f4ad565f24292497a1ef813f8988c5de4b63507
Mark <markmcrg9@gmail.com>
2025-07-01 05:23:24 +0800

    Implemented working prototype
    Changes:
    - Added loading after clicking "Search Topics"
    - Added initial topic hierarchy tree
    - Added search functionality and fetching of data from HF Spaces

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import os
+from gradio_client import Client
+
+app = FastAPI()
+
+app.mount("/src", StaticFiles(directory="src"), name="src")
+
+# Set up Jinja2 templates (using current directory for HTML files)
+templates = Jinja2Templates(directory=".")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("search.html", {"request": request})
+
+@app.post("/search", response_class=HTMLResponse)
+async def search_topics(request: Request, search_term: str = Form(...)):
+    client = Client("markmcrg/juristopic-backend")
+    result = client.predict(
+        search_term=search_term,
+        api_name="/predict"
+    )
+    results_dict = {
+        "topic_name": result[0],
+        "top_words": result[1],
+        "rep_docs": result[2]
+    }
+    return templates.TemplateResponse("results.html", {"request": request, **results_dict})
