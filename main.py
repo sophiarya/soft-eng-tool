f62871bfe6294b09e5e9c1944f6f0343dbd936c4
Mark <markmcrg9@gmail.com>
2025-07-01 05:41:14 +0800

    Added logging

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import os
 from gradio_client import Client
+import pprint
+
 
 app = FastAPI()
 
@@ -18,14 +20,21 @@ async def read_root(request: Request):
 
 @app.post("/search", response_class=HTMLResponse)
 async def search_topics(request: Request, search_term: str = Form(...)):
+    print(f"[LOG] Received search_term: {search_term}")
+
     client = Client("markmcrg/juristopic-backend")
     result = client.predict(
         search_term=search_term,
         api_name="/predict"
     )
+
     results_dict = {
         "topic_name": result[0],
         "top_words": result[1],
         "rep_docs": result[2]
     }
+
+    print("[LOG] Parsed results_dict:")
+    pprint.pprint(results_dict, indent=2, width=100)
+
     return templates.TemplateResponse("results.html", {"request": request, **results_dict})
