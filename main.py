2ef43d6edb0c392b8a9d48de243c7388a4c9db9b
markmcrg <markmcrg9@gmail.com>
2025-09-23 23:25:09 +0800

    Moved styling files to /static folder

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ app = FastAPI()
 # serve files in the `src` folder (images, topic_hierarchy.html, etc.)
 app.mount("/src", StaticFiles(directory="src"), name="src")
 
-# point at project root for static files
-app.mount("/static", StaticFiles(directory="."), name="static")
+# serve CSS, JS and other static assets from the dedicated `static/` folder
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Set up Jinja2 templates (using current directory for HTML files)
 templates = Jinja2Templates(directory=".")
